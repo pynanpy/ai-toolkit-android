93c4508309e45fd3040a91adc9c30c431122d8a1
pynanpy <xiaoyunyuan88@gmail.com>
2026-08-15 17:42:46 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
-implementation("androidx.compose.material:material-icons-extended")
 
 implementation("androidx.datastore:datastore-preferences:1.1.7")
     debugImplementation("androidx.compose.ui:ui-tooling")
